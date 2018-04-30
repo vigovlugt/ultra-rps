283b97def187a633276ca2dba6c78361957def7e
Unknown <vigo@vlugt.net>
2018-04-30 13:27:15 +0200

    SocketIO

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import socketIOClient from 'socket.io-client'
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+     const socket = this.socket = socketIOClient("http://localhost:3001")
+     socket.emit("choice",0)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">Ultra-RPS</span>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }

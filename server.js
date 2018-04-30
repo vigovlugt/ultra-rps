c4a22268e90806c8a6d5dfc2cf1ed78d0174bd07
vigovlugt <vigo@vlugt.net>
2018-04-30 19:03:57 +0200

    backup

--- server.js
+++ server.js
@@ -2,27 +2,51 @@ const express = require('express')
 const http = require('http')
 const socketIO = require('socket.io')
 
-// our localhost port
 const port = 3001
 
 const app = express()
 
-// our server instance
 const server = http.createServer(app)
 
-// This creates our socket using the instance of the server
+app.get('*', function(req, res) {  
+    res.sendFile(path.join( __dirname, './src/index.html'));
+});
+
 const io = socketIO(server)
 
-// This is what the socket.io syntax is like, we will work this later
+let lobbys = [{"owner":"test"}];//{}
+let games = [];
+
 io.on('connection', socket => {
   console.log('User connected')
+  socket.emit('lobbys',lobbys);
 
-  socket.on('choice',(choice)=>{
-    console.log(choice)
+  socket.on("createLobby",(data)=>{
+    if(lobbys.some((lobby)=>{ return lobby.owner === socket.id }))
+        return;
+    lobbys.push({"owner":socket.id,"name":data.name});
+    io.emit('lobbys',lobbys);
+  });
+
+  socket.on("joinLobby",(data)=>{
+    if( data.lobbyOwner == socket.id)
+        return;
+
+    games.push({players:[data.lobbyOwner,socket.id], stage: "choose", gameStart: Date.now()})
+
+    lobbys = lobbys.filter((lobby)=>{return data.lobbyOwner != lobby.owner || socket.id != data.lobbyOwner})
+    io.emit('lobbys',lobbys);
+  })
+
+  //GAME CODE
+  socket.on('chooseItem',(data)=>{
+    console.log(data);
   });
   
   socket.on('disconnect', () => {
     console.log('user disconnected')
+    lobbys = lobbys.filter((lobby)=>{return lobby.owner != socket.id})
+    io.emit('lobbys',lobbys);
   });
 })
 
--- src/App.js
+++ src/App.js
@@ -1,21 +1,115 @@
 import React, { Component } from 'react';
 import socketIOClient from 'socket.io-client'
 import './App.css';
+import { setInterval, log } from 'core-js/library/web/timers';
+import { clearInterval } from 'timers';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {socket:null,lobbys:[],game:{stage:0,gameStart:Date.now(),timeSinceStart:0}}
+  }
+
   componentDidMount(){
-     const socket = this.socket = socketIOClient("http://localhost:3001")
+     const socket = socketIOClient("http://localhost:3001")
+     this.setState({socket:socket});
      socket.emit("choice",0)
+     socket.on("lobbys",(lobbys)=>{
+       this.setState({"lobbys":lobbys})
+     })
+    //  let progressInterval = setInterval(()=>{
+    //    let game = this.state.game;
+    //    game.timeSinceStart = ((Date.now() - game.gameStart) + 15) / 1000; 
+    //    this.setState(game);
+    //    //this.forceUpdate()
+    //    console.log(this.state.game.timeSinceStart);
+    //    if(this.state.game.timeSinceStart >= 15){
+        //  clearInterval(progressInterval);
+    //    }
+    //  },1/60 * 1000)
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header navbar navbar-light bg-light">
+        <header className="App-header navbar navbar-light bg-light" style={{marginBottom:"20px"}}>
         <span className="navbar-brand mb-0 h1">Ultra-RPS</span>
         </header>
+
+        <div className="row" style={{marginLeft:"0px",marginRight:"0px"}}>
+          <div className="col-1"></div>
+          <div className="col-8">
+            <div className="card bg-light">
+              <div className="card-body">
+                {this.state.game == null ?
+                  (//SHOW STATS
+                    null
+                  )
+                  : this.state.game.stage === 0 ?
+                  (//GAME
+                    <div>
+                      <div className="row">
+                      {
+                        ["Rock","Paper","Scissors"].map((item,index)=>{
+                          return (
+                            <div className="col-4" key={index}>
+                              <button className="btn" onClick={()=>{this.chooseItem(index)}} style={{width:"100%",height:"232px"}}>{item}</button>
+                            </div>
+                          )
+                        })
+                      }
+                      </div>
+                      <div className="row" style={{marginTop:"20px",marginLeft:"0px",marginRight:"0px"}}>
+                        <div className="progress" style={{width:"100%"}}>
+                          <div className="progress-bar" role="progressbar" style={{width:this.state.game.timeSinceStart * (100/15) +"%"}} aria-valuenow={this.state.game.timeSinceStart * (100/15)} aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                  :
+                  (//SHOW GAME END
+                    null
+                  )
+                }
+              </div>
+            </div>
+          </div>
+          <div className="col-3">{/* LOBBY MENU */}
+            <div className="card">
+              <div className="card-body">
+                <button className="btn btn-primary" style={{width:"100%"}} onClick={this.createLobby.bind(this)}>Create a lobby</button>
+                <div className="row mt-3"/>
+                <ul className="list-group">
+                  {this.state.lobbys.map((lobby,index)=>{
+                    return (
+                    <li className="list-group-item" key={index}>
+                      <span style={{fontSize:"20px"}}>  
+                        {lobby.owner} 
+                      </span>
+                      {this.state.socket.id == lobby.owner ? null :
+                        (
+                        <button className="btn btn-primary" style={{width:"60px",float:"right",padding:"3px 12px"}}>
+                          Join
+                        </button>
+                        )
+                      }
+                    </li>)
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
+  }
+
+  createLobby(){
+    this.state.socket.emit("createLobby",{})
+  }
+
+  chooseItem(item){
+    this.state.socket.emit("chooseItem",item);
   }
 }
 
